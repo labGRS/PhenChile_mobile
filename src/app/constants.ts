884a212a8f4c677b9224a7a8ba7940d44a78dcb1
Roberto Carvajal <roberto.carvajal@gmail.com>
2024-10-16 15:55:39 -0300

    chore: security update for nextjs, using https for phenproxy

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,1 @@
-export const PROXY_URL = process.env.NEXT_PUBLIC_PROXY_URL || "http://mobilephenchile.pucv.cl/phenproxy.php?path=";
+export const PROXY_URL = process.env.NEXT_PUBLIC_PROXY_URL || "https://mobilephenchile.pucv.cl/phenproxy.php?path=";
