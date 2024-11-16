5625222ebd578a2978f37b2555ffda5d2267665c
Roberto Carvajal <roberto.carvajal@gmail.com>
2024-11-15 21:42:09 -0300

    feat: header and footer
    Signed-off-by: Roberto Carvajal <roberto.carvajal@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import React, { Suspense } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +17,59 @@ export default function RootLayout({
 }>) {
   return (
     <html data-theme="cupcake">
-      <body className={inter.className}>
-        <div className="md:container md:mx-auto pt-10">
-          <Suspense>{children}</Suspense>
+    <head>
+        <link type="text/css" rel="stylesheet" href="https://www.pucv.cl/uuaa/css/v1/reset.css"/>
+        <link type="text/css" rel="stylesheet" href="https://www.pucv.cl/uuaa/css/v1/global.css"/>
+        <link type="text/css" rel="stylesheet" href="https://www.pucv.cl/uuaa/css/v1/superfish.css"/>
+        <link type="text/css" rel="stylesheet" href="https://www.pucv.cl/uuaa/css/v1/unidad_academica.css"/>
+    </head>
+    <body className={inter.className}>
+    <div className="md:container md:mx-auto pt-10">
+        <Suspense>{children}</Suspense>
+    </div>
+    <footer id="footer">
+        <div className="auxi">
+            <div className="banner285">
+
+            </div>
+
+            <h2 className="titular-portada">
+                <div className="table-responsive" data-responsive="yes">
+                    <table style={{width: '100%', height: '189px'}} cellPadding="1" border={0}>
+                        <tbody>
+                        <tr style={{height: '189px'}}>
+                            <td style={{height: '189px'}}>
+                                <p style={{textAlign: 'center'}}><img
+                                    src="https://www.pucv.cl/uuaa/site/artic/20200122/imag/foto_0000000620200122102436/Footer_low_grey_2023v2.png"
+                                    className="responsive-img" width="100%" height="auto"/></p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </h2>
+            <h2 className="titular-portada"><p style={{textAlign: 'center'}}><strong>Instituto de Geografía | Pontificia
+                Universidad Católica de Valparaíso</strong></p></h2>
+
+            <div className="txt-footer">
+                <p>
+                    <img style={{display: 'inline'}} src="https://www.pucv.cl/pucv/site/artic/20150603/imag/foto_0000000120150603180447.png"
+                         id="foto_00000001" className="fotodrag" alt="Imagen foto_00000001"/> Avenida Brasil N°2241
+                    Valparaíso,
+                    Chile
+                    <img style={{display: 'inline'}} src="https://www.pucv.cl/pucv/site/artic/20150603/imag/foto_0000000220150603180447.png"
+                         id="foto_00000002" className="fotodrag" alt="Imagen foto_00000002"/> + 56 32 227 4090
+
+                </p>
+                <div className="separa"></div>
+            </div>
+
+
+            <div className="separa"></div>
         </div>
-      </body>
+        <div className="separa"></div>
+    </footer>
+    </body>
     </html>
   );
 }
